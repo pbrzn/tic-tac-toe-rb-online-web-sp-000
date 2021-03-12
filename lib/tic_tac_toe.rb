019bcdf5365666f1701e24e090ad4eee19fdb747
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-12 19:04:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,7 +56,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player(board))
     display_board(board)
   elsif !valid_move?(board, index) && !over?(board)
     turn(board)
