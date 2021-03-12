8479c18b804ee802c11f97f910af8325e37456a8
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-12 19:33:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,8 +18,8 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board, index)
-  board[index]=current_player(board)
+def move(board, index, value)
+  board[index]=value
 end
 
 def position_taken?(board, index)
@@ -56,7 +56,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, value)
     display_board(board)
   elsif !valid_move?(board, index) && !over?(board)
     turn(board)
